cddfcd2b240b7524cdb175ac4b85c0d75609cd6a
Stoyan Spasov <spasovstoyan@gmail.com>
2021-04-14 02:15:38 +0300

    adding new project

--- src/components/ProjectPage/data.js
+++ src/components/ProjectPage/data.js
@@ -2,22 +2,24 @@ import React from "react"
 import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
 
 export default [
+
     {
         id: 1,
+        img: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        webSite: 'https://giggsi-react-beach-resort.netlify.app/',
+        title: "Abu Dhabi beach resort",
+        text: `Abu Dhabi beach resort web site. Begin your memorable family vacation at all-suite Beach Resort. Whether you are travelling for a weekend getaway, business or family vacation our all suite hotel puts you in the center of it all.`,
+        githubUrl: 'https://github.com/GIGGSI/react-beach-resort'
+    },
+
+    {
+        id: 2,
         img: 'https://images.unsplash.com/photo-1547652577-b4fe2f34d7ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
         webSite: 'https://corojo-club.netlify.app/#/home',
         title: "Corojo Club",
         text: `Corojo Club is the evolution of a passion for cigars and an evolution of them.
         The essence of cigars is explained in a simple sentence by Don Carlos Fuente - "It's not about cigars, it's about people."`,
         githubUrl: 'https://github.com/GIGGSI'
-    },
-    {
-        id: 2,
-        img: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        webSite: 'https://giggsi-react-beach-resort.netlify.app/',
-        title: "Abu Dhabi beach resort",
-        text: `Abu Dhabi beach resort web site. Begin your memorable family vacation at all-suite Beach Resort. Whether you are travelling for a weekend getaway, business or family vacation our all suite hotel puts you in the center of it all.`,
-        githubUrl: 'https://github.com/GIGGSI/react-beach-resort'
     },
 
     {
@@ -44,6 +46,15 @@ export default [
         title: "Backroads Tour Company",
         text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
         githubUrl: 'https://github.com/GIGGSI/Backroads-Tour-Company'
+    },
+    {
+        id: 6,
+        img: 'https://raw.githubusercontent.com/GIGGSI/Search-Github-Users/bf876dffb10890860c1f91760dca53a47ef92090/src/images/login-img.svg',
+        webSite: 'https://giggsi-search-github-users.netlify.app/',
+        title: 'Search Github Users',
+        text: 'App that allows the visitor to search get info about GitHub users.',
+        githubUrl: 'https://github.com/GIGGSI/Search-Github-Users'
+
     },
 
 
--- src/components/latestWork/latesWorkData.js
+++ src/components/latestWork/latesWorkData.js
@@ -3,12 +3,13 @@ import React from "react"
 export default [
     {
         id: 1,
-        webSite: 'https://corojo-club.netlify.app/#/home',
+        webSite: 'https://giggsi-react-beach-resort.netlify.app/',
         className: 'project-1',
-        imageSrc: 'https://images.unsplash.com/photo-1547652577-b4fe2f34d7ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        projectTitle: "corojo club",
-        projectInfo: `Corojo Club is the evolution of a passion for cigars and an evolution of them.`,
+        imageSrc: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        projectTitle: "Abu Dhabi beach resort",
+        projectInfo: `Abu Dhabi beach resort web site.`,
     },
+
     {
         id: 2,
         webSite: 'https://giggsi.github.io/Backroads-Tour-Company/',
@@ -28,11 +29,12 @@ export default [
     },
     {
         id: 4,
-        webSite: 'https://giggsi-react-beach-resort.netlify.app/',
+        webSite: 'https://giggsi-search-github-users.netlify.app/',
         className: 'project-4',
-        imageSrc: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        projectTitle: "Abu Dhabi beach resort",
-        projectInfo: `Abu Dhabi beach resort web site.`,
-    }
+        imageSrc: 'https://raw.githubusercontent.com/GIGGSI/Search-Github-Users/bf876dffb10890860c1f91760dca53a47ef92090/src/images/login-img.svg',
+        projectTitle: "Search Github Users",
+        projectInfo: `App that allows the visitor to search get info about GitHub users.
+        `,
+    },
 
 ]
