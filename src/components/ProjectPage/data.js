b72705b85e1c1fe65d36b91af12d2a792b89b819
Stoyan Spasov <spasovstoyan@gmail.com>
2021-04-23 13:03:11 +0300

    adding new data

--- src/components/ProjectPage/data.js
+++ src/components/ProjectPage/data.js
@@ -15,15 +15,34 @@ export default [
     {
         id: 2,
         img: 'https://images.unsplash.com/photo-1547652577-b4fe2f34d7ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        webSite: 'https://corojo-club.netlify.app/#/home',
+        webSite: 'https://giggsi-corojo-club.netlify.app/',
         title: "Corojo Club",
         text: `Corojo Club is the evolution of a passion for cigars and an evolution of them.
         The essence of cigars is explained in a simple sentence by Don Carlos Fuente - "It's not about cigars, it's about people."`,
-        githubUrl: 'https://github.com/GIGGSI'
+        githubUrl: 'https://github.com/GIGGSI/corojo-club'
     },
-
     {
         id: 3,
+        img: 'http://www.prime-france-num.com/abcbarber/wp-content/uploads/2019/10/cta.jpg',
+        webSite: 'https://gentleman-s-barber-shop.netlify.app/',
+        title: "Gentleman's Barber Shop",
+        text: `
+        Quality service in a unique atmosphere
+For over 5 years we have been helping you maintain your beard and always look your best. Come to our barber shop and try it.
+        `,
+        githubUrl: 'https://github.com/GIGGSI/Gentleman-s-Barber-Shop'
+    },
+    {
+        id: 4,
+        img: 'https://raw.githubusercontent.com/GIGGSI/Search-Github-Users/bf876dffb10890860c1f91760dca53a47ef92090/src/images/login-img.svg',
+        webSite: 'https://giggsi-search-github-users.netlify.app/',
+        title: 'Search Github Users',
+        text: 'App that allows the visitor to search get info about GitHub users.',
+        githubUrl: 'https://github.com/GIGGSI/Search-Github-Users'
+
+    },
+    {
+        id: 5,
         img: 'https://images.unsplash.com/photo-1582793988951-9aed5509eb97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
         webSite: 'https://project-tea.netlify.app/',
         title: "Tea website",
@@ -31,7 +50,7 @@ export default [
         githubUrl: 'https://github.com/GIGGSI'
     },
     {
-        id: 4,
+        id: 6,
         img: 'https://images.unsplash.com/photo-1585499583264-491df5142e83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
         webSite: 'https://flexingsocks.netlify.app/',
         title: "Flexing Socks",
@@ -40,22 +59,12 @@ export default [
         githubUrl: 'https://github.com/GIGGSI/flexingSocks'
     },
     {
-        id: 5,
+        id: 7,
         img: 'https://images.unsplash.com/photo-1516051733211-2492fab1d3a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80',
         webSite: 'https://giggsi.github.io/Backroads-Tour-Company/',
         title: "Backroads Tour Company",
         text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
         githubUrl: 'https://github.com/GIGGSI/Backroads-Tour-Company'
-    },
-    {
-        id: 6,
-        img: 'https://raw.githubusercontent.com/GIGGSI/Search-Github-Users/bf876dffb10890860c1f91760dca53a47ef92090/src/images/login-img.svg',
-        webSite: 'https://giggsi-search-github-users.netlify.app/',
-        title: 'Search Github Users',
-        text: 'App that allows the visitor to search get info about GitHub users.',
-        githubUrl: 'https://github.com/GIGGSI/Search-Github-Users'
-
-    },
-
+    }
 
 ]
--- src/components/latestWork/latesWorkData.js
+++ src/components/latestWork/latesWorkData.js
@@ -9,24 +9,26 @@ export default [
         projectTitle: "Abu Dhabi beach resort",
         projectInfo: `Abu Dhabi beach resort web site.`,
     },
-
     {
         id: 2,
-        webSite: 'https://giggsi.github.io/Backroads-Tour-Company/',
+        webSite: 'https://giggsi-corojo-club.netlify.app/',
         className: 'project-2',
-        imageSrc: 'https://images.unsplash.com/photo-1516051733211-2492fab1d3a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80',
-        projectTitle: "Backroads Tour Company",
-        projectInfo: `I'm baby meditation tofu chillwave`,
+        imageSrc: 'https://images.unsplash.com/photo-1547652577-b4fe2f34d7ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        projectTitle: "Corojo Club",
+        projectInfo: `Torojo Club is the evolution of a passion for cigars and an evolution of them.
+        The essence of cigars is explained in a simple sentence by Don Carlos Fuente - "It's not about cigars, it's about people."`,
     },
+
     {
         id: 3,
-        webSite: 'https://flexingsocks.netlify.app/',
+        webSite: 'https://gentleman-s-barber-shop.netlify.app/',
         className: 'project-3',
-        imageSrc: 'https://images.unsplash.com/photo-1585499583264-491df5142e83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        projectTitle: "Flexing Socks",
-        projectInfo: `The socks we love!
-        Are you looking for your new favorite models?`,
+        imageSrc: 'http://www.prime-france-num.com/abcbarber/wp-content/uploads/2019/10/cta.jpg',
+        projectTitle: "Gentleman's Barber Shop",
+        projectInfo: `
+        We will maintain your beard and you always look your best.`,
     },
+
     {
         id: 4,
         webSite: 'https://giggsi-search-github-users.netlify.app/',
